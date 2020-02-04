fa9c47ebb40e787f794e9827732207bcf960fd6f
usarb <chilatsergiu@gmail.com>
2020-02-04 14:18:41 +0200

    2. DOM. eventListener
    * Query Selector
    * timer

--- 2-DOM.Evenimente/js/app.js
+++ 2-DOM.Evenimente/js/app.js
@@ -3,3 +3,36 @@ document.getElementById('calculateBut').addEventListener("click", function(){
 	const y = Number(document.getElementById('y').value);
 	document.getElementById('result').innerHTML = x + y;
 });
+
+document.getElementById("colorListBut").addEventListener("click", function(){
+	const listItems = document.querySelectorAll("ul.writers li");
+	console.log(listItems);
+
+	listItems.forEach(function(listItem) {
+	  listItem.style.backgroundColor = "green";
+	});
+});
+
+
+document.querySelectorAll("ul.disciplines li").forEach(function(element){
+	element.addEventListener("click", function(){
+		element.style.backgroundColor = "green";
+	});
+});
+
+
+setTimeout(function(){
+	const oldValue = +document.getElementById("counter").value;
+	const newValue = oldValue + 1;
+	document.getElementById("counter").value = newValue;
+}, 1000);
+
+
+setInterval(function(){
+	const oldValue = +document.getElementById("counterCountinuous").value;
+	if(oldValue < 10){
+		const newValue = oldValue + 1;
+		document.getElementById("counterCountinuous").value = newValue;
+	}
+	
+}, 1000);
